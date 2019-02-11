415ac91437cf2c75d71f666b08864a3032cb0159
beccadilts <46534697+beccadilts@users.noreply.github.com>
2019-02-10 21:55:29 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// Interactivity demo 2
+
+//Zoom the Moon
+
+console.log('hello')
+//variable
+var body  = document.querySelector('body')
+var button =document.querySelector('.trigger')
+
+
+//event/event listener
+
+button.addEventListener('click',zoom)
+
+//declare function
+function zoom() {
+	
+	//condition statement v
+if (body.id == 'zoom') {
+	body.id = ''
+} else {
+	body.id = 'zoom'
+}
+
+
+}
